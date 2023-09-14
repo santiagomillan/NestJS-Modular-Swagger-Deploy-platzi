eefdde78c7dc57ecae5f09b78d37648f6f5c149b
smillanpardo <smillan_ext@emergya.com>
2023-09-14 17:42:24 -0500

    tipado y validacion de datos .env. clase 12

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,34 @@
 /* eslint-disable prettier/prettier */
 import { Module, HttpModule, HttpService } from '@nestjs/common';
+import * as Joi from 'joi';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { DatabaseModule } from './database/database.module';
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule } from '@nestjs/config';
+import { enviroments } from './../enviroments';
+import config from './../config';
+
 
 @Module({
-  imports: [HttpModule, UsersModule, ProductsModule, DatabaseModule, ConfigModule.forRoot({
-    envFilePath: '.env',
-    isGlobal: true,
-  })],
+  imports: [
+    HttpModule,
+    UsersModule,
+    ProductsModule,
+    DatabaseModule,
+    ConfigModule.forRoot({
+      envFilePath: enviroments[process.env.NODE_ENV] || '.env',
+      load: [config],
+      isGlobal: true,
+      validationSchema: Joi.object({
+        API_KEY: Joi.number().required(),
+        DATABASE_NAME: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+      })
+    }),
+  ],
   controllers: [AppController],
   providers: [
     AppService,
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,14 +1,18 @@
 /* eslint-disable prettier/prettier */
 import { Injectable, Inject } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import config from 'config'; './../config';
 
 @Injectable()
 export class AppService {
   constructor(
-    @Inject('API_KEY') private apiKey: string,
+    // @Inject('API_KEY') private apiKey: string,
     @Inject('TASKS') private tasks: any[],
+    @Inject(config.KEY) private configService: ConfigType<typeof config>
   ) {}
   getHello(): string {
-    console.log(this.tasks)
-    return `Hello World! ${this.apiKey}`;
+    const api = this.configService.apiKey;
+    const bd = this.configService.database.name;
+    return `Hello World! api: ${api}, bd:  ${bd}`;
   }
 }
